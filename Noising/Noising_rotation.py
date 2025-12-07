417ae2d418be48c274259bad021b4addccc0d939
sondonghan20221993 <20221993@edu.hanbat.ac.kr>
2025-12-07 16:35:18 +0900

    refactor: use __main__
    멀티프로세스에서 __main__ 부분 코드 실행되지않게 선언

--- Noising/Noising_rotation.py
+++ Noising/Noising_rotation.py
@@ -91,8 +91,8 @@ def apply_noise_to_dataset(input_dir, output_dir, noise_level=0.1):
             cv2.imwrite(os.path.join(save_dir, fname), noisy)
 
     print(f"✅ 모든 이미지에 픽셀 단위 노이즈 적용 완료! (noise_level={noise_level})")
-
-# ---------------- 사용 예시 ----------------
-input_dir = "dataset"          # 원본 폴더
-output_dir = "dataset_rotated"   # 노이즈 추가된 폴더
-apply_noise_to_dataset(input_dir, output_dir, noise_level=0.3)
+if __name__ == "__main__":
+    # ---------------- 사용 예시 ----------------
+    input_dir = "dataset"          # 원본 폴더
+    output_dir = "dataset_rotated"   # 노이즈 추가된 폴더
+    apply_noise_to_dataset(input_dir, output_dir, noise_level=0.3)
